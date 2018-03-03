fd4ff51b96b5745260ec40cae0d63c761601fa6b
victor77dev <victor77.dev@gmail.com>
2018-03-03 16:47:35 +0800

    Support cross domain request

--- apiServer.js
+++ apiServer.js
@@ -6,4 +6,11 @@ port = process.env.PORT || 4000
 app.listen(port, () => console.log("Creating app listening on port " + port))
 
 var getJsonResponse = require("./getJsonResponse")
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/api", getJsonResponse)
